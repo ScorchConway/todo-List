8781cfde8577f833b25a852dbfc334f641be0636
M Scott Conner <m.scott.conner@gmail.com>
2017-01-09 15:33:15 -0700

    first commit to add featList
    seeing if plunker is part of the problem by loading this onto the
    actual site, and comparing the behavior.

--- JavaScripts/todoList.js
+++ JavaScripts/todoList.js
@@ -1,117 +1,123 @@
+//todoList has functions to modify a 'todo list'
+//like add, display, change, delete, toggle item completion, toggle, all item completion
+
+var featList;
+
+function preload() {
+  var featlist = $.getJSON("featList.json");
+}
+
 var todoList = {
   todos: [],
   addTodo: function(todoText) {
     this.todos.push({
-      todoText: todoText,
-      completed: false
+      todoText: todoText,  //this is where todoText is defined for each element in todos[]
+      completed: false    //this is where completed is defined for each element in todos[]
     });
   },
-  changeTodo: function(position, todoText) {
-    this.todos[position].todoText = todoText;
+  changeTodo: function(index, todoText) {
+    this.todos[index].todoText = todoText;
   },
-  deleteTodo: function(position) {
-    this.todos.splice(position, 1);
-  },
-  toggleCompleted: function(position) {
-    var todo = this.todos[position];
+  toggleCompleted: function(index) {
+    var todo = this.todos[index];
     todo.completed = !todo.completed;
   },
+  deleteTodo: function(index) {
+    this.todos.splice(index, 1);
+  },
   toggleAll: function() {
-    var totalTodos = this.todos.length;
+    var allTodos = this.todos.length;
     var completedTodos = 0;
-
-    // Get number of completed todos.
-    this.todos.forEach(function(todo) {
-      if(todo.completed === true) {
+    //get completedTodos
+    for (var i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].completed === true) {
         completedTodos++;
       }
-    });
-
-    this.todos.forEach(function(todo){
-      // Case 1: If everything’s true, make everything false.
-      if (completedTodos === totalTodos) {
-        todo.completed = false;
-      // Case 2: Otherwise, make everything true.
-      } else {
-        todo.completed = true;
+    }
+    //Case 1: if all todos are completed (true), make them incomplete (false)
+    if (allTodos === completedTodos) {
+      for (var i = 0; i < this.todos.length; i++) {
+        this.todos[i].completed = false;
       }
-    });
+      //Case 2: otherwise, make them completed (true)
+    } else {
+      for (var i = 0; i < this.todos.length; i++) {
+        this.todos[i].completed = true;
+      }
+    }
   }
 };
 
 var handlers = {
+  toggleAll: function() {
+    todoList.toggleAll();
+    view.displayTodos();
+  },
   addTodo: function() {
-    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    var addTodoTextInput = document.getElementById('addTodoTextInput-demo');
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value = '';
     view.displayTodos();
   },
   changeTodo: function() {
-    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
-    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
-    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
-    changeTodoPositionInput.value = '';
+    var changeTodoIndexInput = document.getElementById('changeTodoNumberInput-demo');
+    var  changeTodoTextInput = document.getElementById('changeTodoTextInput-demo');
+    todoList.changeTodo(changeTodoNumberInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoIndexInput.value = '';
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function(position) {
-    todoList.deleteTodo(position);
+  deleteTodo: function() {
+    var deleteTodoNumberInput = document.getElementById('deleteTodoNumberInput-demo');
+    todoList.deleteTodo(deleteTodoNumberInput.valueAsNumber);
+    deleteTodoNumberInput.value = '';
     view.displayTodos();
   },
   toggleCompleted: function() {
-    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
-    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
-    toggleCompletedPositionInput.value = '';
+    var toggleCompletedNumberInput = document.getElementById('toggleCompletedNumberInput-demo');
+    todoList.toggleCompleted(toggleCompletedNumberInput.valueAsNumber);
+    toggleCompletedNumberInput.value = '';
     view.displayTodos();
   },
-  toggleAll: function() {
-    todoList.toggleAll();
-    view.displayTodos();
+
+
+  loadFeatList: function() {
+    console.log(featList);
+
+    var ul = document.createElement('ul');
+    var featLi = document.createElement('li');
+
+
+    featLi.innerHTML = featList[0].todoText;
+    ul.appendChild(featLi);
+    document.getElementById('feat-list').appendChild(ul);
+
+
+
+
+
   }
+
+
 };
 
 var view = {
   displayTodos: function() {
-    var todosUl = document.querySelector('ul');
+    var todosUl = document.getElementById('demo-list');
     todosUl.innerHTML = '';
-
-    todoList.todos.forEach(function(todo, position) {
-
+    for(var i = 0; i < todoList.todos.length; i++) {
       var todoLi = document.createElement('li');
+      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
-      if (todo.completed === true) {
-         todoTextWithCompletion = '(x) ' + todo.todoText;
-       } else {
-         todoTextWithCompletion = '( ) ' + todo.todoText;
-       }
-
-       todoLi.id = position;
-       todoLi.textContent = todoTextWithCompletion;
-       todoLi.appendChild(this.createDeleteButton());
-       todosUl.appendChild(todoLi);
-    }, this);
-  },
-  createDeleteButton: function() {
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.className = 'deleteButton';
-    return deleteButton;
-  },
-  setUpEventListeners: function() {
-    var todosUl = document.querySelector('ul');
-    todosUl.addEventListener('click', function(click) {
-
-      //get elementClicked
-      var elementClicked = click.target;
-
-      //is elementClicked a delete button?
-      if(elementClicked.className === 'deleteButton') {
-        //run handlers.deleteTodo(position).
-        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      if(todo.completed === true) {
+        todoTextWithCompletion = ('(x) ' + todo.todoText);
+      } else {
+        todoTextWithCompletion = ('( ) ' + todo.todoText);
       }
-    });
+
+      todoLi.textContent = todoTextWithCompletion;
+      todosUl.appendChild(todoLi);
+    }
   }
 };
-
-view.setUpEventListeners();
